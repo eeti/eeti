721b9f51af7f9e021d5c7f97be9eb0f33849221f
eeti.me <sw@eeti.me>
2015-06-21 10:55:51 -0400

    add settings page, add ability to name uploads after originals

---
+++ includes/getsettings.php
@@ -0,0 +1,21 @@
+<?php
+	require_once("includes/settings.inc.php");
+	$settings = array();
+
+	$k=file_get_contents(constant("EETI_USER_PREFS"));
+	$k=explode("\n", $k);
+	for( $i=0; $i<count($k); $i++ ){
+		$tmp = explode(",", $k[$i]);
+		if( $tmp[0] == $_SESSION['user'] ){
+			$settings['set'] = true;
+			if( ! @isset($tmp[1]) || $tmp[1] == "0" ) $settings['fn'] = false;
+			else $settings['fn'] = true;
+		}
+	}
+
+	// use defaults if none exist
+	if( ! $settings['set'] ){
+		$settings['set'] = true;
+		$settings['fn'] = false;
+	}
+?>

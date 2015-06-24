f99aeb37b80e7c2226a3ea454d8906c244fc407f
eeti.me <sw@eeti.me>
2015-06-24 16:18:25 -0400

    don't echo out password when using mkpasswd

--- mkpasswd.php
+++ mkpasswd.php
@@ -3,6 +3,5 @@
 	include("includes/settings.inc.php");
 	unset($argv[0]);
 	$argv=array_values($argv);
-	echo join(" ", $argv) . "\t";
 	echo hash('sha256', EETI_SALT . join(" ", $argv)) . "\n";
 ?>
